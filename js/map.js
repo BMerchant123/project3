270f91a11ce77b5d9ff82eb7ec18a66925ae2ef9
BMerchant <112279397+BMerchant123@users.noreply.github.com>
2022-11-22 19:09:16 -0600

    Adding more content for the google map

--- js/map.js
+++ js/map.js
@@ -1,29 +1,59 @@
-function initMap() {
-	 new google.maps.Map(document.getElementById("map"), {
-    mapId: "8e0a97af9386fef",
-    center: { lat: 41.948006, lng: 87.688036},
-    zoom: 12,
-	};
+
+let map;: google.maps.Map;
+
+function initMap(): void {
+	var centerMap = {
+      zoom: 4,
+      center: {lat: 41.948006, lng: 87.688036},
+    }
+	
+	const map = new google.maps.Map(document.getElementById("map") as HTMLElement, centerMap);
+	
 	const DHCinfo = "<h3>Dave's hot chickent</h3>"
 	
 	const DHCMarker = new google.maps.Marker({
-		position: new google.maps.LatLng(41.948006, 87.688036);
-		title: "Dave's Hot Chicken";
+		position: new google.maps.LatLng(41.948006, 87.688036),
+		title: "Dave's Hot Chicken",
 	});
 	
-	const DHCInfo = new google.maps.InfoWindow({
+	const DHCWindow = new google.maps.InfoWindow({
 		content: DHCinfo,
-		ariaLabel: "Dave's Hot Chicken"
-		
-	new google.maps.Marker({
-		position: new google.maps.LatLng(41.998277, 87.688539);
-		label: "Khan BBQ";
+		ariaLabel: "Dave's Hot Chicken",
+	)};
+	
+	DHCMarker.addListner("click", () => {
+      DHCWindow.open({
+        anchor: DHCMarker,
+        map,
+	  )};
+    });
+	
+	const KBBQMarker = new google.maps.Marker({
+		position: new google.maps.LatLng(41.998277, 87.688539),
+		title: "Khan BBQ",
 	});
 	
-	new google.maps.Marker({
-		position: new google.maps.LatLng(41.998813, 87.690471);
-		label: "Spinzer";
+	const KBBQinfo = "<h3>test</h3>"
+	
+	const KBBQWindow = new google.maps.InfoWindow({
+		content: KBBQinfo,
+		ariaLabel: "Khan BBQ",
 	});
+	
+	KBBQMarker.addListner("click", () => {
+      KKBQWIndow.open({
+        anchor: KBBQMarker,
+        map,
+	  )};
+    });
+	
+	
+	const SMarker = new google.maps.Marker({
+		position: new google.maps.LatLng(41.998813, 87.690471),
+		label: "Spinzer",
+	});
+
 }
+
 
 window.initMap = initMap;
